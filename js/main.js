b261d54b5279490d661b08d275865ef0cde938df
jonathankim407 <jonathankim407@gmail.com>
2017-07-12 18:49:39 -0400

    press .signin button
    fade in modal window

---
+++ js/main.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function(event) {
+
+  var signIn = document.querySelector('.signin');
+  var modal = document.querySelector('.modal');
+
+  signIn.addEventListener('click', function(event) {
+
+    modal.style.display = 'block';
+
+  });
+
+});
